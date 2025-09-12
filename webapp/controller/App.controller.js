9502d44495497fa8c7b84ba030561e4c98130fea
Yohann30Elias <yriond30@gmail.com>
2025-09-12 10:31:33 +0200

    Shellbar redesign

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,17 +1,19 @@
-// webapp/controller/App.controller.js
-sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], function (Controller) {
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
   "use strict";
 
   return Controller.extend("agilh.controller.App", {
-    onTopNavLink: function (e) {
-      const cd = (e.getSource().getCustomData() || []).find(function (c) {
+
+    onMenuSelect: function (e) {
+      var item = e.getParameter("item");
+      var cd = (item.getCustomData() || []).find(function (c) {
         return c.getKey && c.getKey() === "route";
       });
       if (cd) {
         this.getOwnerComponent().getRouter().navTo(cd.getValue());
       }
-    }
+    },
+
+    onSearchPressed: function () { /* später: Suche */ },
+    onAvatarPressed: function () { /* später: Profil-Popover */ }
   });
 });
